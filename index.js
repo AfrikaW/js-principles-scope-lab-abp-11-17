107ba69f1afeeffbd8e2816e201add319fdc98fe
afrika.williams@gmail.com <afrika.williams@gmail.com>
2017-11-12 05:50:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 var customerName = "bob";
 
 function upperCaseCustomerName(customerName) {
-  customerName='customerName'.toUpperCase()
+  customerName= "bob".toUpperCase()
   return customerName;
 }
